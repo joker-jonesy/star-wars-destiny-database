6ef59abf5ea6cf6a5f6fc463d306d5b8b38d4e01
spashmcgrath94 <spashmcgrath94@yahoo.com>
2019-11-26 13:06:09 -0500

    Updates

--- precache-manifest.bfdefeb860812ae40499b30129091c1a.js
+++ precache-manifest.bfdefeb860812ae40499b30129091c1a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "448388e5ed5a8325bac1881113c49383",
+    "revision": "f59948a527fbc9490aa1425e4e9ec0ab",
     "url": "/star-wars-destiny-database/index.html"
   },
   {
-    "revision": "82d01b919be717b1fb87",
-    "url": "/star-wars-destiny-database/static/css/main.cd96ad76.chunk.css"
+    "revision": "e4b6a36c0605f06e7865",
+    "url": "/star-wars-destiny-database/static/css/main.8c0be70f.chunk.css"
   },
   {
     "revision": "e429652a7a2560c991cd",
     "url": "/star-wars-destiny-database/static/js/2.351642a2.chunk.js"
   },
   {
-    "revision": "82d01b919be717b1fb87",
-    "url": "/star-wars-destiny-database/static/js/main.4ae91dc8.chunk.js"
+    "revision": "e4b6a36c0605f06e7865",
+    "url": "/star-wars-destiny-database/static/js/main.794cba06.chunk.js"
   },
   {
     "revision": "f6f415a01d1d9c3b23ad",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-wars-destiny-database/precache-manifest.1fe479ae73a6571e4bd5cccc3fd86e20.js"
+  "/star-wars-destiny-database/precache-manifest.bfdefeb860812ae40499b30129091c1a.js"
 );
 
 self.addEventListener('message', (event) => {
